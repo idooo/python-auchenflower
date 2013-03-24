3fea303a954e9a7cef63b7ebce6e67cb92411862
Alex Shteinikov <alex.shteinikov@gmail.com>
2013-03-24 13:20:19 +1000

    setup file

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+	name='python-auchenflower',
+	version='0.0.4',
+	author='Alex Shteinikov',
+	author_email='alex.shteinikov@gmail.com',
+	url='https://github.com/idooo/python-auchenflower',
+	license='LICENSE.txt',
+	description='Python Auchenflower is simply framework for fast creating web applications.',
+	long_description=open('README.txt').read(),
+	install_requires=[
+		"jinja2 >= 2.6",
+		"cherrypy >= 3.2.2",
+		],
+	)
